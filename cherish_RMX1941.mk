0c1401fcf32ccd48ffd938e212f39a848f37db3f
S0UMY@-GH0$H <shadowsg0877@gmail.com>
2026-08-23 16:41:45 +0530

    Add support for quick tap and maintainer info

--- cherish_RMX1941.mk
+++ cherish_RMX1941.mk
@@ -45,6 +45,10 @@ TARGET_BOOT_ANIMATION_RES := 720
 # CherishOS Stuff
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_INCLUDE_PIXEL_CHARGER  := true
+TARGET_SUPPORTS_QUICK_TAP  := true
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.cherish.maintainer=Shadow-Arc01(§ØÙMŸÅ)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := RMX1941
