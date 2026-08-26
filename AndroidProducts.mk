234e62454bd86b44c84127dc147c36fc269572df
S0UMY@-GH0$H <shadowsg0877@gmail.com>
2026-08-26 10:29:03 +0530

    .......

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/superior_RMX1941.mk
+	$(LOCAL_DIR)/lineage_RMX1941.mk
 
 COMMON_LUNCH_CHOICES := \
-        superior_RMX1941-eng \
-        superior_RMX1941-user \
-        superior_RMX1941-userdebug
+        lineage_RMX1941-eng \
+        lineage_RMX1941-user \
+        lineage_RMX1941-userdebug
